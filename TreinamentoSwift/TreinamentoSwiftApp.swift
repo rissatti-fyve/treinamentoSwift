7d2f283b9c7c82b9354dd81a7f2613b30d860516
Noah Rissatti <noah.rissatti@fyve.com.br>
2023-09-11 12:47:16 -0300

    Initial Commit

---
+++ TreinamentoSwift/TreinamentoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreinamentoSwiftApp.swift
+//  TreinamentoSwift
+//
+//  Created by Noah Rissatti Menezes Rodrigues on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TreinamentoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
